0dc753e6e2ccfc37ae3a07f255732e660facaf9f
Lucie TAN <lucie.tan23@gmail.com>
2021-02-28 14:15:38 +0100

    inscription + connexion et mise en place de firebase

--- app/src/main/java/com/example/moveurfiak/MainActivity.java
+++ app/src/main/java/com/example/moveurfiak/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.moveurfiak;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chargement);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void deconnexion(View view){
+        FirebaseAuth.getInstance().signOut(); // Déconnexion de l'utilisateur
+        startActivity(new Intent(getApplicationContext(), Login.class));
+        finish();
+    }
+
+    public void inscription(View view){
+        startActivity(new Intent(getApplicationContext(), Register.class));
+        finish();
+    }
+
+    public void connexion(View view){
+        startActivity(new Intent(getApplicationContext(), Login.class));
+        finish();
     }
 }
