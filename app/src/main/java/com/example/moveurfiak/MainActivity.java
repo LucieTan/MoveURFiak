dfe9ab65b5751503db06f8a5f1021d4bacb603cf
Yoann <noan2323@gmail.com>
2021-02-28 18:46:28 +0100

    Calcul a continué v3

--- app/src/main/java/com/example/moveurfiak/MainActivity.java
+++ app/src/main/java/com/example/moveurfiak/MainActivity.java
@@ -3,18 +3,27 @@ package com.example.moveurfiak;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+   // Button btn_jeu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*btn_jeu = findViewById(R.id.btn_jeu);
+        btn_jeu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });*/
 
     }
 }
