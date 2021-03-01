177ced873a3545febea4108d4ecb4552335804a7
KaciRayan <moussi.rayan@hotmail.com>
2021-03-02 00:45:29 +0100

    g fé marché le jeu lol

--- app/src/main/java/com/example/moveurfiak/MainActivity.java
+++ app/src/main/java/com/example/moveurfiak/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.moveurfiak;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_play;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chargement);
+        setContentView(R.layout.activity_main);
+        btn_play.findViewById(R.id.btn_play);
+        btn_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent activity = new Intent(getApplicationContext(), MeteoClickerActivity.class);
+                startActivity(activity);
+                finish();
+            }
+        });
     }
 }
