24a51d907bb575e8fae1c0f1ff815cde614c4e78
Lucie TAN <lucie.tan23@gmail.com>
2021-03-03 23:59:34 +0100

    reveil

---
+++ app/src/main/java/com/example/moveurfiak/AlarmService.java
@@ -0,0 +1,44 @@
+package com.example.moveurfiak;
+
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+import com.example.alarm_manager_test.R;
+
+public class AlarmService extends IntentService {
+    private NotificationManager alarmNotificationManager;
+
+    public AlarmService() {
+        super("AlarmService");
+    }
+
+    @Override
+    public void onHandleIntent(Intent intent) {
+        sendNotification("Wake Up! Wake Up!");
+    }
+
+    private void sendNotification(String msg) {
+        Log.d("AlarmService", "Preparing to send notification...: " + msg);
+        alarmNotificationManager = (NotificationManager) this
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, AlarmActivity.class), 0);
+
+        NotificationCompat.Builder alarmNotificationBuilder = new NotificationCompat.Builder(
+                this).setContentTitle("Alarm").setSmallIcon(R.drawable.ic_launcher_background)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
+                .setContentText(msg);
+
+
+        alarmNotificationBuilder.setContentIntent(contentIntent);
+        alarmNotificationManager.notify(1, alarmNotificationBuilder.build());
+        Log.d("AlarmService", "Notification sent.");
+    }
+}
