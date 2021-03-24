19c6eb066d621792682fe266308a181165aef185
Lucie TAN <lucie.tan23@gmail.com>
2021-03-24 13:37:36 +0100

    profil

--- app/src/main/java/application/ProfilActivity.java
+++ app/src/main/java/application/ProfilActivity.java
@@ -14,16 +14,23 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.moveurfiak.Login;
 import com.example.moveurfiak.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 import java.util.Locale;
 
 public class ProfilActivity extends AppCompatActivity {
-
 
 
     @Override
